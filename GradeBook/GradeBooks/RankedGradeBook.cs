19c010f98b62831baee0a24c663933c55c747c2f
iVazovsky <36818037+iVazovsky@users.noreply.github.com>
2019-01-08 17:15:48 +0100

    Update RankedGradeBook.cs

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -30,9 +30,6 @@ namespace GradeBook.GradeBooks
                 return 'D';
             else
                 return 'F';
-
-
-            return base.GetLetterGrade(averageGrade);
         }
     }
 }
